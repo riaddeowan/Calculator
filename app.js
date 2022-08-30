7fc84461419cbd6627e69a46b5cbd8960fa52d5c
riaddeowan <102739530+riaddeowan@users.noreply.github.com>
2022-08-30 22:52:33 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+let result = document.getElementById("result");
+
+function numShow(num){
+   result.value += num;
+}
+
+function clrAll(){
+  result.value = "";
+}
+
+function del(){
+  result.value = result.value.slice(0, -1);
+}
+
+function calculate(){
+  let x = result.value;
+  let y = eval(x);
+
+  result.value = y;
+}
